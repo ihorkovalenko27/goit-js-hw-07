5d0dfa864a59ac1f24f7deb50ed19e8bb17315c5
Ihor Kovalenko <kovalyan27@gmail.com>
2021-06-03 00:02:39 +0300

    Task 1

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const countById = document.querySelector('#categories');
+// console.log('В списке', countById.children.length, 'категории');
+
+const searchTitle = document.querySelectorAll('.item h2');
+// searchTitle.forEach(title => console.log('Категория:', title.textContent));
+
+const countByCategories = document.querySelectorAll('.item ul');
+// countByCategories.forEach(category =>
+//   console.log('Количество элементов:', category.children.length),
+// );
