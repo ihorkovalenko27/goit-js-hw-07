4198e7810e58982ee75c93ca1e921e3e965b3b2e
Ihor Kovalenko <kovalyan27@gmail.com>
2021-06-03 18:40:01 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const newLi = ingredients.map(ingredient => {
+  const madeLi = document.createElement('li');
+  madeLi.innerText = ingredient;
+  return madeLi;
+});
+
+const listIngredients = document.querySelector('#ingredients');
+
+listIngredients.append(...newLi);
+
+console.log(listIngredients);
