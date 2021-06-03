0dc153bbbb588720be2316e673470ced291ef7a3
Ihor Kovalenko <kovalyan27@gmail.com>
2021-06-03 18:15:47 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,12 @@
 const countById = document.querySelector('#categories');
 console.log('В списке', countById.children.length, 'категории');
 
-const searchTitle = document.querySelectorAll('.item h2');
-searchTitle.forEach(title => console.log('Категория:', title.textContent));
-
-const countByCategories = document.querySelectorAll('.item ul');
-countByCategories.forEach(category =>
-  console.log('Количество элементов:', category.children.length),
+const searchElement = document.querySelectorAll('.item');
+searchElement.forEach(el =>
+  console.log(
+    'Категория:',
+    el.firstElementChild.textContent,
+    'Количество элементов:',
+    el.lastElementChild.children.length,
+  ),
 );
