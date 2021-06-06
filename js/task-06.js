2a1b08fef4ab6c25913226cafa881572e7db1902
Ihor Kovalenko <kovalyan27@gmail.com>
2021-06-06 15:35:02 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const getInput = document.querySelector('input');
 const getDataLength = getInput.getAttribute('data-length');
 
 getInput.addEventListener('blur', () => {
-  if (getDataLength) {
+  if (getInput.value.length === +getDataLength) {
     getInput.classList.add('valid');
     getInput.classList.remove('invalid');
   }
